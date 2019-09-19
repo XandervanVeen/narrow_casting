82ae9052b6c0b6d15c15e828321a9deec0038e57
Xander van Veen <xander.media.klas@gmail.com>
2019-09-19 11:13:03 +0200

    Styling to agenda on index

--- newsInfo.php
+++ newsInfo.php
@@ -13,8 +13,6 @@
 $json = file_get_contents('https://newsapi.org/v2/top-headlines?country=nl&apiKey=1b5ffc1eac5a417fa9974cb1a606cf51');
 $obj = json_decode($json);
 $data = json_decode(json_encode($obj), TRUE);
-//echo "<h1>Status: {$data['status']}</h1>";
-//echo "<h3>Total Results: {$data['totalResults']}</h3>";
 $totalArticles = count($data['articles']);
 for ($i = 0; $i < $totalArticles; $i++){
     echo "<div class='newsContent'>";
@@ -24,7 +22,7 @@ for ($i = 0; $i < $totalArticles; $i++){
         echo "</div>";
         echo "<div class='newsImg'>";
             if ($data['articles'][$i]['urlToImage'] == ""){
-                echo "<img style='width: 150px; height: 120px; padding: 10% 0% 10% 0%;' src='img/news-placeholder.png'>";
+                echo "<a href='{$data['articles'][$i]['url']}'><img style='width: 150px; height: 120px; padding: 10% 0% 10% 0%;' src='img/news-placeholder.png'></a>";
             }
             else {
                 echo "<a href='{$data['articles'][$i]['url']}'><img style='padding: 0% 10% 0% 10%;' src='{$data['articles'][$i]['urlToImage']}'></img></a>";
